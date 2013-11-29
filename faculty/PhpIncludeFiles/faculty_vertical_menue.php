64e30f99f122e5aaba1ae9402def340e7875f222
Kunal Khandelwal <k.khandelwal@iitg.ernet.in>
2013-11-30 00:45:35 +0530

    ok

---
+++ faculty/PhpIncludeFiles/faculty_vertical_menue.php
@@ -0,0 +1,28 @@
+
+<div id="adminVMenuItem1">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/faculty/index.php'?>">Home</a>
+</div>
+<div id="adminVMenuItem2">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/faculty/change_pass.php'?>">Change Password</a>
+</div>
+<div id="adminVMenuItem3">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/faculty/logout.php'?>">Log Out</a>
+</div>
+<div id="adminVMenuItem4">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/Backend/Records/upload_history.php'?>">Notice Board</a>
+</div>
+<div id="adminVMenuItem5">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/Backend/Schedule/schedule_list.php'?>">Messages</a>
+</div>
+<div id="adminVMenuItem6">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/Backend/permission_mt.php'?>">Discussion Forum</a>
+</div>
+<div id="adminVMenuItem7">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/Backend/permission_bt.php'?>">B.Tech Permissions</a>
+</div>
+<div id="adminVMenuItem8">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/Backend/logout.php'?>">Log Out</a>
+</div>
+<div id="adminVMenuItem9">
+	<a class="adminVMenuItemLInk" href="<?php echo constant("HOST11").'/people/admin_people.php'?>">People</a>
+</div>
